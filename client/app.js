84fa56f9ec062bf9668aa90e35264c9a5a792191
LeonidR <leo2410@yandex.ru>
2016-02-08 16:01:41 +0200

    Added filter for capitalizing first letters

--- client/app.js
+++ client/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('ToDoManager', ['dndLists', 'ngMaterial']);
+
+app.filter('capitalize', function() {
+    return function(input, $scope) {
+        if (input!=null)
+            input = input.toLowerCase();
+        return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+});
