e95d9c43155a2d80caa64573ebffbad6041de263
LeonidR <leo2410@yandex.ru>
2016-02-08 16:02:23 +0200

    Added function for updating tasks

--- client/rest-api/controllers/TasksController.php
+++ client/rest-api/controllers/TasksController.php
@@ -29,8 +29,8 @@ class TasksController extends ActiveController
             $newTaskOrder = $request->post('newTaskOrder');
             $numItems = count($newTaskOrder);
             foreach ($newTaskOrder as $k => $v) {
-                $task = Task::findOne($v['id']);
-                $task->task_order = $v['task_order'];
+                $task = Task::findOne($v['taskId']);
+                $task->task_order = $v['taskOrder'];
                 $task->update();
                 if (($k+1) === $numItems) {
                     if ($task->update() !== false) {
@@ -43,4 +43,56 @@ class TasksController extends ActiveController
         }
         return false;
     }
+
+    public function actionCreateNewTask()
+    {
+        $date = date('Y/m/d', time());
+        $request = Yii::$app->request;
+        if ($request->isPost) {
+            $newTask = $request->post('newTask');
+            $task = new Task();
+            $task->title         =   $newTask['taskTitle'];
+            $task->description   =   $newTask['taskDescription'];
+            $task->priority      =   $newTask['taskPriority'];
+            $task->due_date      =   date('Y-m-d', strtotime($newTask['taskDueDate']));
+            $task->create_date   =   $date;
+            $task->project_id    =   $newTask['projectId'];
+            $task->task_order    =   $newTask['taskOrder'];
+            $task->complited     =   0;
+            $task->save();
+
+            if ($task->save() !== false) {
+                return 'Task saved successfully';
+            } else {
+                return 'Task save: failed!';
+            }
+        }
+        return false;
+    }
+
+    public function actionUpdateTask()
+    {
+        $request = Yii::$app->request;
+        if ($request->isPost) {
+            $newTask = $request->post('newTask');
+            $task = Task::findOne($newTask['id']);
+            $task->title         =   $newTask['title'];
+            $task->description   =   $newTask['description'];
+            $task->priority      =   $newTask['priority'];
+            $task->due_date      =   date('Y-m-d', strtotime($newTask['due_date']));
+            $task->create_date   =   $newTask['create_date'];
+            $task->project_id    =   $newTask['project_id'];
+            $task->task_order    =   $newTask['task_order'];
+            $task->complited     =   $newTask['complited'];
+            $task->update();
+
+            if ($task->update() !== false) {
+                return 'Task updated successfully';
+            } else {
+                return 'Task update: failed!';
+            }
+        }
+        return false;
+    }
+
 }
